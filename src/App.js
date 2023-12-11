3c21eed792462f2a91b52dd4dc647bb535d3fbb3
TheShwetabhSaxena <theshwetabhsaxena@gmail.com>
2023-12-11 16:44:55 +0530

    calculator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useRef} from 'react'
+import Button from './components/Button';
 
 function App() {
+  const [result, changeResult] = useState(0)
+  const inputSpace = useRef(null)
+  const style = {
+    display: "flex",
+  }
+  const commmonButtonStyle = {
+    borderStyle: "Solid",
+    color: "white",
+    backgroundColor: "black"
+  }
+  const uniqueButtonStyle = {
+    borderStyle: "Solid",
+    color: "white",
+    backgroundColor: "orange"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Simplest working calculator</h1>
+      <div>
+        <label>{result}</label>
+        <br/>
+        <input ref = {inputSpace} type='text'></input>
+        <br/>
+        <div style={style}>
+          <Button title = "Add" style = {commmonButtonStyle} function = {e => {
+            e.preventDefault()
+            changeResult((result) => result+Number(inputSpace.current.value))
+          }}></Button>
+          <Button title = "Subtract" style = {commmonButtonStyle} function = {e => {
+            e.preventDefault()
+            changeResult((result) => result-Number(inputSpace.current.value))
+          }}></Button>
+          <Button title = "Multiply" style = {commmonButtonStyle} function = {e => {
+            e.preventDefault()
+            changeResult((result) => result*Number(inputSpace.current.value))
+          }}></Button>
+          <Button title = "Divide" style = {commmonButtonStyle} function = {e => {
+            try {
+              e.preventDefault()
+              changeResult((result) => result/Number(inputSpace.current.value))
+            }
+            catch (error) {
+              e.preventDefault()
+              changeResult(0)
+              changeResult('Error: Division by zero')
+            }
+          }}></Button>
+          <Button title = "ResetInput" style = {uniqueButtonStyle} function = {e => {
+            e.preventDefault()
+            inputSpace.current.value = ''
+          }}></Button>
+          <Button title = "ResetResult" style = {uniqueButtonStyle} function = {e => {
+            e.preventDefault()
+            changeResult(0)
+          }}></Button>
+        </div>
+      </div>
     </div>
   );
 }
